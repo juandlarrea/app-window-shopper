5e3a2c62d04f367eb968b4f318ceb5efbc685b64
Juan Larrea <juan@Juans-MacBook-Air.local>
2018-07-07 14:17:07 -0400

    Started UI and first text field

---
+++ App-Window-Shopper/App-Window-Shopper/View/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  App-Window-Shopper
+//
+//  Created by Juan Larrea on 7/6/18.
+//  Copyright © 2018 Juan Larrea. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
